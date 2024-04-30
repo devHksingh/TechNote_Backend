3c0384c0235a626535bd64ba1f690b95ed2b488f
devHksingh <hksingh.dev@gmail.com>
2024-04-30 23:11:27 +0530

    changes in server.ts [ add logic to connect DB]

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 import app from "./src/app";
 import { config } from "./src/config/config";
+import connectDB from "./src/config/db";
 
 const startServer = async ()=>{
     // connect database
-
+    await connectDB()
     const port = config.port
 
     app.listen(port,()=>{
