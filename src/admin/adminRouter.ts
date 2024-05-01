0b0eaea2508d1ae3745a1c6b86e550b69583170a
devHksingh <hksingh.dev@gmail.com>
2024-05-01 22:09:16 +0530

    add express validator to route register

--- src/admin/adminRouter.ts
+++ src/admin/adminRouter.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import { createAdmin, loginAdmin, logoutAdmin } from './adminController'
+import { adminRegisterSchema } from './admin_register_schema'
+import validate_Admin_req_schema from '../middlewares/validate_Admin_register_req_schema'
 
 const  adminRouter = express.Router()
 
 // routes
 
-adminRouter.post('/register',createAdmin)
+adminRouter.post('/register',
+                adminRegisterSchema,
+                validate_Admin_req_schema,
+                createAdmin)
 adminRouter.post('/login',loginAdmin)
 adminRouter.post('/logout',logoutAdmin)
 
