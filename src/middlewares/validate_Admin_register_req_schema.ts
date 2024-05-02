7da3c0adb013e91b9b408e25e1b0624a92a55c8b
devHksingh <hksingh.dev@gmail.com>
2024-05-02 17:57:52 +0530

    express validator

--- src/middlewares/validate_Admin_register_req_schema.ts
+++ src/middlewares/validate_Admin_register_req_schema.ts
@@ -6,8 +6,10 @@ import createHttpError from "http-errors";
 const validate_Admin_req_schema = (req:Request,res:Response,next:NextFunction)=>{
     const errors = validationResult(req)
     if(!errors.isEmpty()){
-        const httpError = errors.array()
-        return next(createHttpError(400,`${httpError}`))
+        const [{msg}] = errors.array()
+        
+        
+        return next(createHttpError(400,`${msg}`))
     }
     next()
 }
