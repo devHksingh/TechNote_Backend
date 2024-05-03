ac81b2831ea6f29ae16b32b4d63d4807970a2b84
devHksingh <hksingh.dev@gmail.com>
2024-05-03 21:45:52 +0530

    fix cloudnary issue

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -2,7 +2,7 @@ import {v2 as cloudinary} from 'cloudinary';
 import { config } from './config';
           
 cloudinary.config({ 
-    cloud_name:config.cloudinaryCloud,
+    cloud_name:config.cloudinaryCloudName,
     api_key:config.cloudinaryApiKey,
     api_secret:config.cloudinaryApiSecret,
 });
