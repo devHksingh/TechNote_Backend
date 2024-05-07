e22f457000d3dcf2fa2a7485bfea92637b9b1630
devHksingh <hksingh.dev@gmail.com>
2024-05-07 15:34:28 +0530

    add user id  and role on request

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -2,13 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import jwt from 'jsonwebtoken'
 import { config } from "../config/config";
+import { Admin } from "../admin/admin.Model";
 
 export interface AuthRequest extends Request{
     userId:String;
     userRole:String;
 }
 
-const authenticate = (req:Request,res:Response,next:NextFunction)=>{
+const authenticate = async(req:Request,res:Response,next:NextFunction)=>{
     const token = req.header('Authorization')
     
     if(!(token && (token?.startsWith('Bearer ')))){
@@ -16,17 +17,22 @@ const authenticate = (req:Request,res:Response,next:NextFunction)=>{
         
         return next(createHttpError(401,'Auth token is required'))
     }
-    // req.header('Authorization') = token.split(' ')[1]
+    
     const userAccessToken = token.split(' ')[1]
       // Verify access token
     try {
         const isValidAndDecoded = jwt.verify(userAccessToken, config.jwtAccessSecret as string) as jwt.JwtPayload;;
         console.log('decoded token',isValidAndDecoded);
-        console.log('decoded token sub',isValidAndDecoded.sub);
+        
         
         const _req = req as AuthRequest
-        // _req.userId = isValidAndDecoded?.id as string
+        
         const {id,role} = isValidAndDecoded
+
+        const adminProfile = await Admin.findById(id).select("-password -refreshToken")
+        // req.user = adminProfile
+        console.log('admin',adminProfile);
+        
         _req.userId = id as string
         _req.userRole = role as string
         next(); // Proceed if verification is successful
