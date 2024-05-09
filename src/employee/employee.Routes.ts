35f1ee977ecf98a11a76e5d63aab4e8221259357
devHksingh <hksingh.dev@gmail.com>
2024-05-09 23:08:42 +0530

    add route for login emp

--- src/employee/employee.Routes.ts
+++ src/employee/employee.Routes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { upload } from '../middlewares/multer.middleware'
-import { createEmployee } from './employee.Controller'
+import { createEmployee, loginEmployee } from './employee.Controller'
 import authenticate from '../middlewares/authenticate'
 import { employeeRegisterSchema } from './employeeRegisterSchema'
 import validate_Employee_req_schema from '../middlewares/validate_Express_Validator_schema'
+import { employeeLoginSchema } from './employee_login_schema'
 
 const employeeRouter = express.Router()
 
@@ -20,6 +21,13 @@ employeeRouter.post(
     validate_Employee_req_schema,
     authenticate,
     createEmployee
+)
+
+employeeRouter.post(
+    '/login',
+    employeeLoginSchema,
+    validate_Employee_req_schema, 
+    loginEmployee
 )
 
 
