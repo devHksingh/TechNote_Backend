fa49300ed8a8f2a6514610c94a30e7f270172a36
devHksingh <hksingh.dev@gmail.com>
2024-05-09 18:03:28 +0530

    add express validator schema for password change

---
+++ src/admin/admin_passwordChange_schema.ts
@@ -0,0 +1,20 @@
+import { body, header } from "express-validator";
+
+
+
+const schema =[
+    body('oldPassword')
+        .isLength({min:4})
+        .withMessage('password is required and must be at least 4 characters long'),
+    body('newPassword')
+        .isLength({min:4})
+        .withMessage('password is required and must be at least 4 characters long'),
+
+    
+    header('Authorization')
+        .isString()   
+        .notEmpty() 
+        
+]
+
+export {schema as adminPasswordChangeSchema}
