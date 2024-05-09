3f7b16a07032340c9e618121f357854b27c3c5cd
devHksingh <hksingh.dev@gmail.com>
2024-05-09 22:45:16 +0530

    add express validation schema

---
+++ src/employee/employeeRegisterSchema.ts
@@ -0,0 +1,30 @@
+import { body, header } from "express-validator";
+
+const schema = [
+    body('name')
+        .isString()
+        .notEmpty()
+        .isLength({min:2})
+        .withMessage('Admin name at least 2 characters long'),
+    body('email')
+        .isEmail()
+        .withMessage('Email must contain a valid email address'),
+    body('password')
+        .isLength({min:4})
+        .withMessage('password is required and must be at least 4 characters long'),
+    body('avatar').optional(),
+    body('role')
+        .isString()
+        .notEmpty()
+        .isLength({min:5}),
+    body('salary')
+        .isString()
+        .notEmpty(),
+        
+    header('Authorization')
+        .isString()   
+        .notEmpty() 
+
+]
+
+export {schema as employeeRegisterSchema}
