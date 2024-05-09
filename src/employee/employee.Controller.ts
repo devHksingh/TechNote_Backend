372e41a1e0ef3ba16528db39c1f03d2c43cb576d
devHksingh <hksingh.dev@gmail.com>
2024-05-09 23:09:01 +0530

    add controller for emp login

--- src/employee/employee.Controller.ts
+++ src/employee/employee.Controller.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { Employee } from "./employee.Model";
 import createHttpError from "http-errors";
-import bcryptPassword from "../utils/bcrytHashpasword";
 import { config } from "../config/config";
 import { uploadOnCloudinary } from "../utils/uploadOnCloudinary";
 import  {genrateAccessToken, genrateRefreshToken } from "../utils/jwtAccessToken";
 import { EmployeeInterface } from "../types/employeeTypes";
 import { AuthRequest } from "../middlewares/authenticate";
+import {decodeAccessTokenAndCheckExpiry, decodeRefreshTokenAndCheckExpiry} from '../utils/decodeJwtTokenAndCheckExpiry'
+import bcryptPassword, { bcryptComparePassword } from "../utils/bcrytHashpasword";
 
 
 const createEmployee = async (req:Request,res:Response,next:NextFunction)=>{
@@ -107,6 +108,107 @@ const createEmployee = async (req:Request,res:Response,next:NextFunction)=>{
     res.status(201).json({message:'Employee is created successfully',accessToken:accessToken,id:newEmployee._id})
 }
 
+const loginEmployee =  async (req:Request,res:Response,next:NextFunction)=>{
+
+    const {email,password} = req.body
+    
+     
+    // verify email is register in DB?
+    let user
+
+    try {
+         user = await Employee.findOne({email:email})
+    
+        if(!user){
+            return next(createHttpError(404,"User not found"))
+    
+        }
+    } catch (error) {
+        return next(createHttpError(500,"DB CONNECTION PROBLEM"))
+    }
+    const userPassowrd = user.password
+    const isMatchPassword = await bcryptComparePassword(password,userPassowrd)
+    if(!isMatchPassword){
+        return next(createHttpError(400,'Invalid Credentials'))
+    }
+        
+    // check access token
+    const authHeader = (req.headers.authorization)?.split(' ')[1]
+    
+    let newAccessToken
+
+    if(!authHeader){
+        console.log('NO ACCESS TOKEN FOUND');
+        
+        newAccessToken = genrateAccessToken({id:user._id,role:user.role})
+    }else{
+        console.log(' ACCESS TOKEN found');
+        
+        const isValidAccessToken = decodeAccessTokenAndCheckExpiry(authHeader as string,{id:user._id,role:user.role})
+        console.log(' ACCESS TOKEN viryfication done');
+    
+        if(isValidAccessToken?.isInvalid){
+            return next(createHttpError(402,`Invalid Token`))
+        }
+        if(isValidAccessToken?.isTokenExp){
+            console.log('EXP ACCESS TOKEN');
+            
+             newAccessToken = isValidAccessToken.accessToken
+        }else if(!isValidAccessToken?.isTokenExp){
+            console.log('access token = auth header');
+            
+            newAccessToken = authHeader
+        }
+
+    }
+    
+    // check refresh token
+
+    const userRefreshToken = user.refreshToken
+
+    if(!userRefreshToken){
+        console.log('NO REFRESH TOKEN FOUND');
+        const newRefreshToken1 = genrateRefreshToken({id:user._id,role:user.role})
+        try {
+            user.refreshToken = newRefreshToken1
+            await user.save({validateBeforeSave:false})
+            console.log('SAVE NEW REFRESH TOKEN ON DB');
+        } catch (error) {
+            next(createHttpError(400,`DB error falied to update refresh token`))
+        }
+    }else{
+        console.log('VALIDATE REFRESH TOKEN ');
+        const isValidRefreshToken= decodeRefreshTokenAndCheckExpiry(user.refreshToken,{id:user._id,role:user.role})
+
+        let newRefreshToken
+    
+        if(isValidRefreshToken?.isInvalid){
+            return next(createHttpError(403,`Invalid Token`))
+        }
+        if(isValidRefreshToken?.isTokenExp){
+                  
+            newRefreshToken = isValidRefreshToken.refreshToken
+            // update new refresh token in db
+            try {
+                user.refreshToken = newRefreshToken
+                         
+                await user.save({validateBeforeSave:false})
+                console.log('REGENRATE REFRESH TOKEN ON DB');
+                
+            } catch (error) {
+                next(createHttpError(400,`DB error falied to update refresh token`))
+            }
+        }
+        
+    }
+
+    
+
+
+    res.status(200).json({message:`${user.role} login successfully`, newAccessToken:newAccessToken,id:user._id})
+}
+
 export {
     createEmployee,
+    loginEmployee,
 }
