55b600a0ffb4331f16599a35fedde9dfcd5bfb56
devHksingh <hksingh.dev@gmail.com>
2024-05-11 19:34:19 +0530

    add minnor changes in client {testing}

--- src/types/clientTypes.ts
+++ src/types/clientTypes.ts
@@ -1,13 +1,14 @@
 import { TaskInterface } from "./taskTypes";
 
-export interface ClientInterface{
+export interface TestClientInterface{
     _id:string;
     name:string;
     email:string;
     password:string;
     avatar:string;
+    role:string;
+    taskHistory:TaskInterface;
     refreshToken:string;
     createdAt:Date;
     updatedAt:Date;
-    // task:TaskInterface;
 }
